44c4e0cab3324d1b5047ce6c5d90d0ce6a54d194
Alelí Schäffer <113878447+Aleli7@users.noreply.github.com>
2022-11-13 15:28:17 -0300

    Update ejercicio-1.js

--- modulo01/tp2/ejercicio-1.js
+++ modulo01/tp2/ejercicio-1.js
@@ -1,11 +1,46 @@
-var verdadero, char
-var valores = [verdadero, false, 2, "hola", "mundo", 3, char];
+var valores = ["verdadero", "false", 2, "hola", "mundo", 3, "char"];
+
 let text = []
 valores.forEach((elem) => {
-if(typeof elem == "string") {
-text.push(elem)
-}
-})
-alert(text.sort((a, b)=>{return a.length - b.length}));
-Function myFunction (array) {
-return array + 2
+    if (typeof elem == "string") {
+        text.push(elem)
+    }
+}); 
+
+alert(text.sort((a, b) => { return a.length - b.length }));
+
+let numbers = []
+valores.forEach((elem) => {
+    if (typeof elem == "number") {
+        numbers.push(elem)
+    }
+});
+
+alert(numbers);
+
+function suma (array){
+    return valores + 7;
+};
+
+function resta (numbers){
+    return valores - 7;
+};
+
+function multiplicacion (numbers){
+   return  valores * 7;
+};
+
+function division (numbers){
+   return valores / 7;
+};
+
+var operacion1 = valores.map(suma);
+var operacion2 = valores.map(resta);
+var operacion3 = valores.map(multiplicacon);
+var operacion4 = valores.map(division);
+
+alert(operacion1);
+alert(operacion2);
+alert(operacion3);
+alert(operacion4);
+
